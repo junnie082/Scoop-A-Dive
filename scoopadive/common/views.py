d41734e008029207e7ef904332cd651b9696a17a
Hyojeong Jun <junnie082@gmail.com>
2024-01-16 09:57:45 +0900

    chore: delete unusable code

--- scoopadive/common/views.py
+++ scoopadive/common/views.py
@@ -1,7 +1,4 @@
-from sqlite3 import IntegrityError
-
 from django.contrib.auth.models import User
-from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
--- scoopadive/user_profile/views.py
+++ scoopadive/user_profile/views.py
@@ -1,8 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
-from .models import Profile
-from common.forms import UserForm
-
 
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
