eaef2f32f2a1f6a106ca1545d28445fe8c52204d
Hyojeong Jun <junnie082@gmail.com>
2024-01-20 21:22:59 +0900

    design: changed UI of detail.html and posting.html

--- scoopadive/board/views.py
+++ scoopadive/board/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
@@ -79,4 +80,11 @@ def modify_post(request, post_id):
     post.save()
 
     return render(request, 'board/detail.html', {'post': post})
+
+@login_required(login_url='common:login')
+def post_vote(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.voter.add(request.user)
+
+    return redirect('board:detail', post_id=post.id)
 
