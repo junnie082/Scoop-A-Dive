4fe1d6de686b3e98530e8447d7ed698376dabf58
Hyojeong Jun <junnie082@gmail.com>
2024-01-27 00:59:53 +0900

    STATIC_ROOT 생성

--- scoopadive/scoopadive/settings/base.py
+++ scoopadive/scoopadive/settings/base.py
@@ -16,6 +16,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+     BASE_DIR / 'static',
+]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
