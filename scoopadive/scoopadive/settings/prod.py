863a1c720876363be6ade58b0b761c52454fa0c9
Hyojeong Jun <junnie082@gmail.com>
2024-01-27 16:45:56 +0900

    chore: BASE_DIR

--- scoopadive/scoopadive/settings/prod.py
+++ scoopadive/scoopadive/settings/prod.py
@@ -1,6 +1,7 @@
 from .base import *
 
 ALLOWED_HOSTS = ['3.36.81.50']
-STATIC_ROOT = BASE_DIR / 'main/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+print("STATIC_ROOT: " + STATIC_ROOT)
 STATICFILES_DIRS = []
 DEBUG = False
